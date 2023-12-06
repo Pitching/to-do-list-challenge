8d6bcc2705f14d3eef47965c9fb211a27a079a40
Pitching <adchir95@gmail.com>
2023-12-06 17:05:51 -0500

    creating the modal for new notes and assigning it to button to pop-up on click

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Modal from './components/Modal';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="d-flex justify-content-center mt-5">
+      <div className="container text-center">
+          <Modal />
+      </div>
     </div>
   );
 }

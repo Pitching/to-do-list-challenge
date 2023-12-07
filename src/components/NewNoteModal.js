dc581f11bed562b89cf0077a4fac792c463ac61f
Pitching <adchir95@gmail.com>
2023-12-06 19:19:08 -0500

    creating the update to the notes state and closing modal on confirmation

--- src/components/NewNoteModal.js
+++ src/components/NewNoteModal.js
@@ -1,15 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect, useState } from 'react';
 
-function NewNoteModal(allNotes, setAllNotes) {
+function NewNoteModal({allNotes, setAllNotes}) {
 
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [date, setDate] = useState("");
 
   useEffect(() => {
-    console.log(title, description, date);
-  },[title, description, date]);
+    console.log(title, description, date, allNotes);
+  },[title, description, date, allNotes]);
 
   function addNote(e) {
     e.preventDefault();
@@ -20,7 +20,7 @@ function NewNoteModal(allNotes, setAllNotes) {
       desc: description,
     }
 
-    setAllNotes([...allNotes].concat(noteItem).reverse())
+    setAllNotes([...allNotes].concat(noteItem))
     setTitle("");
     setDescription("");
     setDate("");
@@ -33,7 +33,7 @@ function NewNoteModal(allNotes, setAllNotes) {
         Create new To-do Note
       </button>
 
-      <form className="modal fade" id="NewNote" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" onSubmit={addNote}>
+      <div className="modal fade" id="NewNote" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div className="modal-dialog modal-dialog-centered">
           <div className="modal-content px-3 py-2">
             <label for="title" className="text-start">Title</label>
@@ -41,14 +41,14 @@ function NewNoteModal(allNotes, setAllNotes) {
             <label for="description" className="text-start mt-3">Description</label>
             <input className="form-control" type="text" id="description" placeholder="Description" aria-label="default input example" required onChange={(e) => setDescription(e.target.value)}></input>
             <label for="date" className="text-start mt-3">Date</label>
-            <input className="form-control" type="date" id="date" placeholder="Description" aria-label="default input example" required onChange={(e) => setDate(e.target.value)}></input>
+            <input className="form-control" type="date" id="date" placeholder="" aria-label="default input example" required onChange={(e) => setDate(e.target.value)}></input>
             <div className="d-flex justify-content-between mt-4">
               <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" className="btn btn-primary" onClick={addNote}>Create</button>
+              <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={addNote}>Create</button>
             </div>
           </div>
         </div>
-      </form>
+      </div>
     </section>
   )
 }

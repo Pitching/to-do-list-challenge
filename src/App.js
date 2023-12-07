060bf60ab2320eebe3b3b36444c645b9ceda97a4
Pitching <adchir95@gmail.com>
2023-12-07 15:31:21 -0500

    adding search functionality to the app

--- src/App.js
+++ src/App.js
@@ -6,35 +6,60 @@ import { useEffect, useState } from 'react';
 
 function App() {
 
-  const [allNotes, setAllNotes] = useState(localStorage.getItem('notes') ?  JSON.parse(localStorage.getItem('notes')) : []);
+  const [allNotes, setAllNotes] = useState(localStorage.getItem('notes') ? JSON.parse(localStorage.getItem('notes')) : []);
   const [noteData, setNoteData] = useState({
     title: "",
     description: "",
     date: "",
     category: ""
   })
+  const [search, setSearch] = useState("");
 
-  function deleteNote (id) {
+  function deleteNote(id) {
     const notesToKeep = allNotes.filter(note => note.id !== id);
     setAllNotes(notesToKeep);
   }
 
+  const filterNotes = allNotes.filter((note) => {
+    const { category, title, description, date } = note;
+    const searchText = search?.toLowerCase();
+    return (
+      category?.toLowerCase().includes(searchText) ||
+      title?.toLowerCase().includes(searchText) ||
+      description?.toLowerCase().includes(searchText) ||
+      (date?.includes(searchText))
+    )
+  })
+
   useEffect(() => {
     localStorage.setItem("notes", JSON.stringify(allNotes));
   }, [allNotes]);
-  
+  useEffect(() => {
+
+  })
+
   return (
     <div className="d-flex justify-content-center">
       <div className="container text-center  mt-5">
-        <NewNoteModal setAllNotes={setAllNotes} allNotes={allNotes} noteData={noteData} setNoteData={setNoteData}/>
-        <ul className="list-group mt-5 align-items-center">
-          {
+        <NewNoteModal setAllNotes={setAllNotes} allNotes={allNotes} noteData={noteData} setNoteData={setNoteData} />
+        <input className="mt-2 rounded" onChange={(e) => setSearch(e.target.value)}>
+        </input>
+        <ul className="list-group mt-3 align-items-center">
+          {filterNotes ?
+            filterNotes.map(note => (
+              <ToDoItem
+                key={note.id}
+                deleteNote={() => { deleteNote(note.id) }}
+                note={note}
+                setAllNotes={setAllNotes} />
+            ))
+            :
             allNotes.map(note => (
-              <ToDoItem 
-              key={note.id} 
-              deleteNote={() => {deleteNote(note.id)}} 
-              note={note}
-              setAllNotes={setAllNotes}/>
+              <ToDoItem
+                key={note.id}
+                deleteNote={() => { deleteNote(note.id) }}
+                note={note}
+                setAllNotes={setAllNotes} />
             ))
           }
         </ul>

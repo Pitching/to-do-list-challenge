ef3bc45407958d76f0ba54ae188b94d86ca52e65
Pitching <adchir95@gmail.com>
2023-12-06 19:04:00 -0500

    creating use states and keyboard capture for debugging

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NewNoteModal from './components/NewNoteModal';
 import ToDoItem from './components/ToDoItem';
+import { useState } from 'react';
 
 function App() {
+
+  const [allNotes, setAllNotes] = useState([]);
+
   return (
     <div className="d-flex justify-content-center mt-5">
       <div className="container text-center">
-        <NewNoteModal />
+        <NewNoteModal setAllNotes={setAllNotes} allNotes={allNotes}/>
         <ul className="list-group mt-5 align-items-center">
-          <ToDoItem />
           <ToDoItem />
         </ul>
       </div>
--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,11 +1,11 @@
 import './ToDoItem.css'
 import { FaTrash } from "react-icons/fa";
 
-function ToDoItem() {
+function ToDoItem({ title, description, date, deleteItem, updateItem}) {
 
   return (
-    <li className="col border-top justify-content-between text-start list-unstyled list-group-item w-50 d-flex" data-bs-toggle="modal" data-bs-target="#EditNote">
-      <div>
+    <li className="col border-top justify-content-between text-start list-unstyled list-group-item w-50 d-flex">
+      <div className="w-100" data-bs-toggle="modal" data-bs-target="#EditNote">
         <h5>
           Title
         </h5>
@@ -13,7 +13,7 @@ function ToDoItem() {
           Description
         </p>
       </div>
-      <div className="d-flex align-items-center">
+      <div className="d-flex align-items-center" onClick={deleteItem}>
         <FaTrash size={30} />
       </div>
 
@@ -21,14 +21,14 @@ function ToDoItem() {
         <div className="modal-dialog modal-dialog-centered">
           <div className="modal-content px-3 py-2">
             <label for="title" className="text-start">Title</label>
-            <input className="form-control" type="text" id="title" placeholder="Title" aria-label="default input example"></input>
+            <input className="form-control" type="text" id="title" placeholder={title} aria-label="default input example"></input>
             <label for="description" className="text-start mt-3">Description</label>
-            <input className="form-control" type="text" id="description" placeholder="Description" aria-label="default input example"></input>
+            <input className="form-control" type="text" id="description" placeholder={description} aria-label="default input example"></input>
             <label for="date" className="text-start mt-3">Date</label>
-            <input className="form-control" type="date" id="date" placeholder="Description" aria-label="default input example"></input>
+            <input className="form-control" type="date" id="date" placeholder={date} aria-label="default input example"></input>
             <div className="d-flex justify-content-between mt-4">
               <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" className="btn btn-primary">Update</button>
+              <button type="button" className="btn btn-primary" onClick={updateItem}>Update</button>
             </div>
           </div>
         </div>

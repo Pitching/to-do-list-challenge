2940cd7dddc4c8fbb690cb5366faaf346278e8e0
Pitching <adchir95@gmail.com>
2023-12-07 17:39:26 -0500

    fixing all warnings

--- src/components/NewNoteModal.js
+++ src/components/NewNoteModal.js
@@ -34,27 +34,27 @@ function NewNoteModal({ allNotes, setAllNotes, noteData, setNoteData }) {
         Create New Note
       </button>
 
-      <form className="modal fade" id="NewNote" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" onSubmit={addNote}>
+      <form className="modal fade" id="NewNote" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" onSubmit={addNote}>
         <div className="modal-dialog modal-dialog-centered">
           <div className="modal-content px-3 py-2">
-            <label for="title" className="text-start">Title</label>
+            <label htmlFor="title" className="text-start">Title</label>
             <input className="form-control" type="text" id="title" placeholder="Title" aria-label="default input example" required onChange={(e) => setNoteData({ ...noteData, title: e.target.value })} value={noteData.title}></input>
-            <label for="description" className="text-start mt-3">Description</label>
+            <label htmlFor="description" className="text-start mt-3">Description</label>
             <input className="form-control" type="text" id="description" placeholder="Description" aria-label="default input example" required onChange={(e) => setNoteData({ ...noteData, description: e.target.value })} value={noteData.description}></input>
-            <label for="date" className="text-start mt-3">Date</label>
+            <label htmlFor="date" className="text-start mt-3">Date</label>
             <input className="form-control" type="date" id="date" placeholder="" aria-label="default input example" required onChange={(e) => setNoteData({ ...noteData, date: e.target.value })} value={noteData.date}></input>
-            <div class="btn-group mt-4">
-              <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" value={noteData.category}>
+            <div className="btn-group mt-4">
+              <button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" value={noteData.category}>
                 {noteData.category ? noteData.category : `Please select an option`}
               </button>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#" onClick={(e) => setNoteData({...noteData, category: e.target.innerText})}>Work</a></li>
-                <li><a class="dropdown-item" href="#" onClick={(e) => setNoteData({...noteData, category: e.target.innerText})}>Personal</a></li>
-                <li><a class="dropdown-item" href="#" onClick={(e) => setNoteData({...noteData, category: e.target.innerText})}>Home</a></li>
-                <li><a class="dropdown-item" href="#" onClick={(e) => setNoteData({...noteData, category: e.target.innerText})}>Family</a></li>
-                <li><a class="dropdown-item" href="#" onClick={(e) => setNoteData({...noteData, category: e.target.innerText})}>Social</a></li>
-                <li><a class="dropdown-item" href="#" onClick={(e) => setNoteData({...noteData, category: e.target.innerText})}>Health</a></li>
-                <li><a class="dropdown-item" href="#" onClick={(e) => setNoteData({...noteData, category: e.target.innerText})}>Hobby</a></li>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#" onClick={(e) => setNoteData({...noteData, category: e.target.innerText})}>Work</a></li>
+                <li><a className="dropdown-item" href="#" onClick={(e) => setNoteData({...noteData, category: e.target.innerText})}>Personal</a></li>
+                <li><a className="dropdown-item" href="#" onClick={(e) => setNoteData({...noteData, category: e.target.innerText})}>Home</a></li>
+                <li><a className="dropdown-item" href="#" onClick={(e) => setNoteData({...noteData, category: e.target.innerText})}>Family</a></li>
+                <li><a className="dropdown-item" href="#" onClick={(e) => setNoteData({...noteData, category: e.target.innerText})}>Social</a></li>
+                <li><a className="dropdown-item" href="#" onClick={(e) => setNoteData({...noteData, category: e.target.innerText})}>Health</a></li>
+                <li><a className="dropdown-item" href="#" onClick={(e) => setNoteData({...noteData, category: e.target.innerText})}>Hobby</a></li>
               </ul>
             </div>
             <div className="d-flex justify-content-between mt-4">
--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -78,7 +78,7 @@ function ToDoItem({ note, deleteNote, setAllNotes }) {
               <div className="mt-2">
                 {note.date}
               </div>
-              <div className>
+              <div>
                 {note.category}
               </div>
             </div>
@@ -91,24 +91,24 @@ function ToDoItem({ note, deleteNote, setAllNotes }) {
         :
         (<form className="w-100" onSubmit={updateNote}>
           <div className="w-100">
-            <label for="title" className="text-start">Title</label>
+            <label htmlFor="title" className="text-start">Title</label>
             <input className="form-control" type="text" id="title" defaultValue={note.title} aria-label="default input example" onChange={(e) => setEditNoteData({ ...editNoteData, title: e.target.value })}></input>
-            <label for="description" className="text-start mt-3">Description</label>
+            <label htmlFor="description" className="text-start mt-3">Description</label>
             <input className="form-control" type="text" id="description" defaultValue={note.description} aria-label="default input example" onChange={(e) => setEditNoteData({ ...editNoteData, description: e.target.value })}></input>
-            <label for="date" className="text-start mt-3">Date</label>
+            <label htmlFor="date" className="text-start mt-3">Date</label>
             <input className="form-control" type="date" id="date" defaultValue={note.date} aria-label="default input example" onChange={(e) => setEditNoteData({ ...editNoteData, date: e.target.value })}></input>
-            <div class="btn-group mt-4 w-100">
-              <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" value={editNoteData.category}>
+            <div className="btn-group mt-4 w-100">
+              <button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" value={editNoteData.category}>
                 {editNoteData.category ? editNoteData.category : `Please select an option`}
               </button>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#" onClick={(e) => setEditNoteData({ ...editNoteData, category: e.target.innerText })}>Work</a></li>
-                <li><a class="dropdown-item" href="#" onClick={(e) => setEditNoteData({ ...editNoteData, category: e.target.innerText })}>Personal</a></li>
-                <li><a class="dropdown-item" href="#" onClick={(e) => setEditNoteData({ ...editNoteData, category: e.target.innerText })}>Home</a></li>
-                <li><a class="dropdown-item" href="#" onClick={(e) => setEditNoteData({ ...editNoteData, category: e.target.innerText })}>Family</a></li>
-                <li><a class="dropdown-item" href="#" onClick={(e) => setEditNoteData({ ...editNoteData, category: e.target.innerText })}>Social</a></li>
-                <li><a class="dropdown-item" href="#" onClick={(e) => setEditNoteData({ ...editNoteData, category: e.target.innerText })}>Health</a></li>
-                <li><a class="dropdown-item" href="#" onClick={(e) => setEditNoteData({ ...editNoteData, category: e.target.innerText })}>Hobby</a></li>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#" onClick={(e) => setEditNoteData({ ...editNoteData, category: e.target.innerText })}>Work</a></li>
+                <li><a className="dropdown-item" href="#" onClick={(e) => setEditNoteData({ ...editNoteData, category: e.target.innerText })}>Personal</a></li>
+                <li><a className="dropdown-item" href="#" onClick={(e) => setEditNoteData({ ...editNoteData, category: e.target.innerText })}>Home</a></li>
+                <li><a className="dropdown-item" href="#" onClick={(e) => setEditNoteData({ ...editNoteData, category: e.target.innerText })}>Family</a></li>
+                <li><a className="dropdown-item" href="#" onClick={(e) => setEditNoteData({ ...editNoteData, category: e.target.innerText })}>Social</a></li>
+                <li><a className="dropdown-item" href="#" onClick={(e) => setEditNoteData({ ...editNoteData, category: e.target.innerText })}>Health</a></li>
+                <li><a className="dropdown-item" href="#" onClick={(e) => setEditNoteData({ ...editNoteData, category: e.target.innerText })}>Hobby</a></li>
               </ul>
             </div>
           </div>
